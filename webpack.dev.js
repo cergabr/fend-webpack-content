0e781db0f4c911c7313c5440b7e8f7755fbd8ae0
cergabr <78414909+cergabr@users.noreply.github.com>
2021-06-03 14:12:50 +0200

    add webpack serve, bundle-analyzer-plugin, clean-plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,17 @@
-const path = require("path") 
-const webpack = require("webpack")
-const htmlWebPackPlugin = require("html-webpack-plugin")
-const ESLintPlugin = require("eslint-webpack-plugin")
+const path = require("path");
+const webpack = require("webpack");
+const htmlWebPackPlugin = require("html-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     mode: "development",
 	entry: "./src/client/index.js",
     devtool: "source-map",
+    devServer: {
+        port: "8000",
+      },
     module:{
         rules: [
             {
@@ -29,6 +34,16 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new ESLintPlugin()
+        new ESLintPlugin(),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+        new BundleAnalyzerPlugin(),
     ]
 }
