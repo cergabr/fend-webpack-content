5ce943c3acfdcf376631c20113a33ea0b595947a
cergabr <gabriele.cerqueglini@gmail.com>
2021-06-11 11:01:50 +0200

    fix doublequotes

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,11 @@
 import { checkForName } from "./js/nameChecker";
 import { handleSubmit } from "./js/formHandler";
 import img from "./img/clouds.png";
-import './styles/resets.scss';
-import './styles/base.scss';
-import './styles/footer.scss';
-import './styles/form.scss';
-import './styles/header.scss';
+import "./styles/resets.scss";
+import "./styles/base.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
 
 console.log(checkForName);
 alert("Hallo");
